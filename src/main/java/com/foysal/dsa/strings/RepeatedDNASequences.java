33953c50312b0b12c913fdec174b686ed627ae8f
bit-based-computing <foysalahmmed73@gmail.com>
2024-07-27 16:25:34 +0600

    Solved: LeetCode 187 - Repeated DNA Sequences

---
+++ src/main/java/com/foysal/dsa/strings/RepeatedDNASequences.java
@@ -0,0 +1,23 @@
+package com.foysal.dsa.strings;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RepeatedDNASequences {   // LeetCode 187
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> set = new HashSet<>();
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i < s.length() - 10; i++) {
+            String dna = s.substring(i, i + 10);
+            if(set.contains(dna)) {
+                result.add(dna);
+            } else {
+                set.add(dna);
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
