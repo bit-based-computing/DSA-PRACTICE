567a661b3284af5cc80ec4b630964a6d1b285bf7
bit-based-computing <foysalahmmed73@gmail.com>
2024-07-27 16:27:56 +0600

    Solved: LeetCode 187 - Repeated DNA Sequences, update code for an edge case

--- src/main/java/com/foysal/dsa/strings/RepeatedDNASequences.java
+++ src/main/java/com/foysal/dsa/strings/RepeatedDNASequences.java
@@ -10,7 +10,7 @@ public class RepeatedDNASequences {   // LeetCode 187
     public List<String> findRepeatedDnaSequences(String s) {
         Set<String> set = new HashSet<>();
         Set<String> result = new HashSet<>();
-        for (int i = 0; i < s.length() - 10; i++) {
+        for (int i = 0; i <= s.length() - 10; i++) {
             String dna = s.substring(i, i + 10);
             if(set.contains(dna)) {
                 result.add(dna);
