7f89cd7d747365ae236cd49310e1640d5db9ece2
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-02 22:02:27 +0600

    Solved : Group Anagrams - LeetCode 49

---
+++ src/main/java/com/foysal/dsa/strings/GroupAnagrams.java
@@ -0,0 +1,20 @@
+package com.foysal.dsa.strings;
+
+import java.util.*;
+
+public class GroupAnagrams {   // LeetCode 49
+
+    List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
