7132207c5502eb7141915798f998ad2386182efa
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-06 20:40:00 +0600

    Solved: LeetCode 242 - Valid Anagram

---
+++ src/main/java/com/foysal/dsa/strings/ValidAnagram.java
@@ -0,0 +1,19 @@
+package com.foysal.dsa.strings;
+
+public class ValidAnagram {  // LeetCode 242
+     public boolean isAnagram(String s, String t) {
+         if (s.length() != t.length()) return false;
+         int[] count = new int[26];
+         for (char c : s.toCharArray()) {
+             count[c - 'a']++;
+         }
+         for (char c : t.toCharArray()) {
+             count[c - 'a']--;
+             if (count[c - 'a'] < 0) return false;
+         }
+         for (int i = 0; i < 26; i++) {
+             if (count[i] != 0) return false;
+         }
+         return true;
+     }
+}
