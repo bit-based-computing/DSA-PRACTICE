ab9dfeb0a38c9a7eb31182d891c54ec0be6da76f
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-13 09:42:36 +0600

    Solved: LeetCode 134 - Gas Station :  added antoher optimized way to solve

--- src/main/java/com/foysal/dsa/arrays/GasStation.java
+++ src/main/java/com/foysal/dsa/arrays/GasStation.java
@@ -21,4 +21,21 @@ public class GasStation {  // LeetCode 134
         }
         return -1;
     }
+
+    // another way
+    public int canCompleteCircuit2(int[] gas, int[] cost) {
+        int len = gas.length;
+        int start = 0;
+        int fuel = 0;
+        int low = 0;
+        for (int i = 0; i < len; i++) {
+            fuel += gas[i] - cost[i];
+            if(fuel < low) {
+                start = i+1;
+                low = fuel;
+            }
+        }
+        if(fuel < 0) return -1;
+        return start % len;
+    }
 }
