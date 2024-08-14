690e291c780cbbde73a60e46a4fc52f8a091ec2a
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-14 10:29:31 +0600

    Solved: LeetCode 41 - First Missing Positive

---
+++ src/main/java/com/foysal/dsa/arrays/FirstMissingPositive.java
@@ -0,0 +1,24 @@
+package com.foysal.dsa.arrays;
+
+public class FirstMissingPositive {  // LeetCode 41
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        for(int i = 0; i < len; i++){
+            if(nums[i] <= 0){
+                nums[i] = len + 1;
+            }
+        }
+
+        for(int i = 0; i < len; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if(index < len && nums[index] > 0){
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < len; i++){
+            if(nums[i] > 0) return i + 1;
+        }
+        return len + 1;
+    }
+}
