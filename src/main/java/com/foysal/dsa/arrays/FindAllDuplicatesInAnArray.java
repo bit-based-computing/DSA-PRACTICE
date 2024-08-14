d124c8462f49afacbbef1c5f221d84ce8bcbe8a5
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-14 09:25:27 +0600

    Solved: LeetCode 442 - Find All Duplicates In An Array : memory optimized way

--- src/main/java/com/foysal/dsa/arrays/FindAllDuplicatesInAnArray.java
+++ src/main/java/com/foysal/dsa/arrays/FindAllDuplicatesInAnArray.java
@@ -16,4 +16,20 @@ public class FindAllDuplicatesInAnArray {  // LeetCode 442
 
         return ans;
     }
+
+    // another way memory efficient
+    public List<Integer> findDuplicates2(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int len = nums.length;
+        for(int i = 0; i < len; i++){
+            int index = Math.abs(nums[i]);
+            if(nums[index-1] < 0){
+                ans.add(index);
+            } else {
+                nums[index - 1] = -nums[index - 1];
+            }
+        }
+
+        return ans;
+    }
 }
