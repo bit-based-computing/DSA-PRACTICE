b4ae4a63e31563e8ade3567a7ed8707d50e37771
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-27 05:49:13 +0600

    Solved: LeetCode 392 - Is Subsequence

---
+++ src/main/java/com/foysal/dsa/two/pointers/IsSubsequence.java
@@ -0,0 +1,19 @@
+package com.foysal.dsa.two.pointers;
+
+public class IsSubsequence {   // LeetCode 392
+
+    public boolean isSubsequence(String s, String t) {
+        int slen = s.length();
+        int tlen = t.length();
+        char[] schar = s.toCharArray();
+        char[] tchar = t.toCharArray();
+        int l = 0;
+        int r = 0;
+        while(l < slen && r < tlen) {
+            if(tchar[r] == schar[l]) l++;
+            r++;
+        }
+        if(l == slen) return true;
+        return false;
+    }
+}
