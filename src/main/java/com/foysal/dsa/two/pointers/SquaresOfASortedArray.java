d1288484ef20b41e8fb9891c7b9bbc5e4a54ee94
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-27 06:25:34 +0600

    Solved: LeetCode 977 - Squares of a Sorted Array

---
+++ src/main/java/com/foysal/dsa/two/pointers/SquaresOfASortedArray.java
@@ -0,0 +1,22 @@
+package com.foysal.dsa.two.pointers;
+
+public class SquaresOfASortedArray {   // LeetCode 977
+
+    public int[] sortedSquares(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        int l = 0;
+        int r = len - 1;
+        int k = len - 1;
+        while (l <= r) {
+            if (Math.abs(nums[l]) <= Math.abs(nums[r])) {
+                ans[k--] = nums[r] * nums[r];
+                r--;
+            } else {
+                ans[k--] = nums[l] * nums[l];
+                l++;
+            }
+        }
+        return ans;
+    }
+}
