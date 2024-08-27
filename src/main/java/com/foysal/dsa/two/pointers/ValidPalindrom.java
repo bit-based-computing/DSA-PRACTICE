f4dfd781aab55217c55cc227c07913ab968c22cf
bit-based-computing <foysalahmmed73@gmail.com>
2024-08-27 07:15:41 +0600

    Solved: LeetCode 125 - Valid Palindrome

---
+++ src/main/java/com/foysal/dsa/two/pointers/ValidPalindrom.java
@@ -0,0 +1,30 @@
+package com.foysal.dsa.two.pointers;
+
+public class ValidPalindrom {  // LeetCode 125
+    public boolean isPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        int l = 0;
+        int r = s.length() - 1;
+        boolean lAlphanumeric = false;
+        boolean rAlphanumeric = false;
+        while (l < r) {
+            lAlphanumeric = isAlphanumeric(chars[l]);
+            rAlphanumeric = isAlphanumeric(chars[r]);
+            if(lAlphanumeric && rAlphanumeric) {
+                if(Character.toLowerCase(chars[l]) != Character.toLowerCase(chars[r]))
+                    return false;
+                l++;
+                r--;
+            } else {
+                if(!lAlphanumeric) l++;
+                if(!rAlphanumeric) r--;
+            }
+        }
+        return true;
+    }
+
+    private boolean isAlphanumeric(char c) {
+        return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z');
+    }
+}
