b02f5ab55345280e14640a7693b50f913870e28f
bit-based-computing <foysalahmmed73@gmail.com>
2024-09-02 08:55:48 +0600

    Solved: LeetCode 7 - Set Matrix Zeroes

---
+++ src/main/java/com/foysal/dsa/arrays/matrix/SetMatrixZeroes.java
@@ -0,0 +1,52 @@
+package com.foysal.dsa.arrays.matrix;
+
+public class SetMatrixZeroes { // LeetCode 73
+
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean hasFirstRow = false;
+        boolean hasFirstCol = false;
+        for (int c = 0; c < col; c++) {
+            if (matrix[0][c] == 0) hasFirstRow = true;
+        }
+        for (int r = 0; r < row; r++) {
+            if(matrix[r][0] == 0) hasFirstCol = true;
+        }
+
+        for (int r = 1; r < row; r++) {
+            for(int c = 1; c < col; c++) {
+                if(matrix[r][c] == 0) {
+                    matrix[0][c] = 0;
+                    matrix[r][0] = 0;
+                }
+            }
+        }
+
+        for (int c = 1; c < col; c++) {
+            if (matrix[0][c] == 0) nullifyColumn(c, matrix);
+        }
+
+        for (int r = 1; r < row; r++){
+            if(matrix[r][0] == 0) nullifyRow(r, matrix);
+        }
+
+        if (hasFirstRow) {
+            nullifyRow(0, matrix);
+        }
+        if (hasFirstCol) {
+            nullifyColumn(0, matrix);
+        }
+
+    }
+
+    private void nullifyColumn(int col, int[][] matrix) {
+        int row = matrix.length;
+        for(int r = 0; r < row; r++) matrix[r][col] = 0;
+    }
+
+    private void nullifyRow(int row, int[][] matrix) {
+        int col = matrix[0].length;
+        for(int c = 0; c < col; c++) matrix[row][c] = 0;
+    }
+}
